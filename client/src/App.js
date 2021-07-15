8d5b930d16e83f43f07a8bb93e0feef53dbb4c1a
arthurs14 <arthurcs426@gmail.com>
2021-07-15 15:13:01 -0700

    starting files

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+
+
+const App = () => {
+  return (
+    <div>
+      Video Chat App
+    </div>
+  );
+};
+
+export default App;
