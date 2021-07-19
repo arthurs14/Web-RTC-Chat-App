2989edf7c16bcbaca08ae1b30073435f2ecbd8bf
arthurs14 <arthurcs426@gmail.com>
2021-07-19 11:16:05 -0700

    acompleted answerCall method

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -10,8 +10,12 @@ const ContextProvider = ({ children }) => {
   const [stream, setStream] = useState(null);
   const [me, setMe] = useState('');
   const [call, setCall] = useState({});
+  const [callAccepted, setCallAccepted] = useState(false);
+  const [callEnded, setCallEnded] = useState(false);
 
   const myVideo = useRef();
+  const userVideo = useRef();
+  const connectionRef = useRef();
 
   useEffect(() => {
     navigator.mediaDevices.getUserMedia({ video: true, audio: true })
@@ -25,9 +29,29 @@ const ContextProvider = ({ children }) => {
     socket.on('calluser', ({ from, name: callerName, signal }) => {
       setCall({ isReceivedCall: true, from, name: callerName, signal });
     });
-  });
+  }, []);
 
-  const answerCall = () => {};
+  const answerCall = () => {
+    setCallAccepted(true);
+
+    // action and handlers when we call someone
+    const peer = new Peer({ initiator: false, trickle: false, stream });
+
+    // connects to socket
+    peer.on('signal', (data) => {
+      socket.emit('answercall', { signal: data, to: call.from });
+    });
+
+    // other person's stream
+    peer.on('stream', currentStream => {
+      userVideo.current.srcObject = currentStream;
+    });
+
+    peer.signal(call.signal);
+
+    // current connection = to the peer inside this connection
+    connectionRef.current = peer;
+  };
 
   const callUser = () => {};
 
